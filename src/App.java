92bb4f0b06c3226ac03a4f5e0c1defd4fe844841
G-T <guillaume.tripier1@gmail.com>
2019-06-04 16:32:58 +0200

    code with couplage

--- src/App.java
+++ src/App.java
@@ -4,13 +4,15 @@ import java.util.List;
 public class App {
 
     private double rigidity;
+    private List<App> apps;
 
     public App(){
         setRigidity(50.0);
     }
 
-    public App(double rigidity){
+    public App(double rigidity, int number){
         setRigidity(rigidity);
+        setApps(number);
     }
 
     public double getRigidity(){
@@ -21,14 +23,27 @@ public class App {
         this.rigidity = rigidity;
     }
 
-    public static void main(String[] args) {
+    public void setApps(int count){
         List<App> apps = new ArrayList<>();
-        for(int i = 0; i < 10; i++){
-            apps.add(new App(60.0 + i));
+        for(int i = 0; i < count; i++){
+            apps.add(new App(20.0 + i, 0));
         }
+        this.apps = apps;
+    }
 
-        for(int i = 0; i < apps.size(); i++){
-            System.out.println(apps.get(i).getRigidity());
+    public List<App> getApps(){
+        return(this.apps);
+    }
+
+    public void printApps(){
+        for(int i = 0; i < getApps().size(); i++){
+            System.out.println(getApps().get(i).getRigidity());
         }
+    }
+
+    public static void main(String[] args) {
+        App app = new App(10, 40);
+        app.printApps();
+
     }
 }
